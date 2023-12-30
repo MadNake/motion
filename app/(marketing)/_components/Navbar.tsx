0588bc14e6d7abf8a323b64d7728e4e101a113ec
MadNake <72208527+MadNake@users.noreply.github.com>
2023-12-30 23:17:39 +0100

    added useScroll

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Logo } from "./Logo"
 import { ModeToggle } from "@/components/mode-togle"
+import { cn } from "@/lib/utils"
+import { useScroll } from "@/hooks/use-scroll"
 
 export const Navbar = () => {
+
+	const scrollPos = useScroll(10);
+
 	return (
-		<div className="flex p-6 fixed top-0 border-b shadow-sm w-full">
+		<div className={cn("flex p-6 fixed top-0 w-full",
+			scrollPos && "border-b shadow-sm")}>
 			<Logo />
 			<div className="flex items-center gap-x-2 justify-between w-full md:justify-end flex-1">
 				<Button
---
+++ hooks/use-scroll.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export const useScroll = (scrollPos = 10) => {
+	const [scrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+
+		const handleScroll = () => {
+			const scroll = window.scrollY > scrollPos ? true : false;
+			setScrolled(scroll);
+		};
+
+		handleScroll();
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll)
+		}
+	}, [scrollPos])
+
+	return scrolled
+}
