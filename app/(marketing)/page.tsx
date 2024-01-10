755b3f6a2395ae3601f29b42e83655487ee2c84c
MadNake <72208527+MadNake@users.noreply.github.com>
2024-01-10 14:51:40 +0100

    fix style

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ import { Footer } from "./_components/Footer";
 
 const MarketingPage = () => {
 	return (
-		<div className="flex flex-col min-h-full">
+		<div className="flex flex-col min-h-full dark:bg-[#1F1F1F]">
 			<div className="min-h-full px-6 flex-1 gap-y-8 flex flex-col pb-10 w-full justify-center md:justify-start items-center  text-center">
 				<Heading />
 				<Heroes />
