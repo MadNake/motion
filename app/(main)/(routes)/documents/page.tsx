ff351e2079e1ea467c5327f180f3ebffd76c7567
MadNake <72208527+MadNake@users.noreply.github.com>
2024-01-24 16:25:41 +0100

    added main page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,40 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+import Image from "next/image";
+
+import { useUser } from "@clerk/clerk-react";
+
 const DocumentsPage = () => {
+
+	const { user } = useUser();
+
 	return (
-		<div>
-			page
+		<div className="relative flex items-center justify-center h-full flex-col space-y-4">
+			<Image
+				src="/empty.png"
+				alt="Emty"
+				height={300}
+				width={300}
+				className="object-contain block dark:hidden"
+			/>
+			<Image
+				src="/empty-dark.png"
+				alt="Emty"
+				height={300}
+				width={300}
+				className="object-contain hidden dark:block"
+			/>
+			<h2
+				className="text-lg font-medium">
+				Welcom to {user?.firstName}&apos;s Motion
+			</h2>
+			<Button>
+				<PlusCircle className="h-4 w-4 mr-2"/>
+				Create a note
+			</Button>
 		</div>
 	)
 }
